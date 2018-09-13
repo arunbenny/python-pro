895881885febc2c8ea94c643ef25084e24339a6b
arunbenny <40994418+arunbenny@users.noreply.github.com>
2018-09-13 23:23:53 +0530

    Update sum of natural numbers.py

--- sum of natural numbers.py
+++ sum of natural numbers.py
@@ -1,6 +1,6 @@
-n=int(input("Enter a number: "))
-sum1 = 0
-while(n > 0):
-    sum1=sum1+n
-    n=n-1
-print("The sum of first n natural numbers is",sum1)
+a=int(input("Enter a number:"))
+s=0
+while(a > 0):
+    s=s+a
+    a=a-1
+print("The sum of first n natural numbers is:",s)
